653a26159816ea7958f903b4d06733d1bb41bf0d
codehiker1997 <faizantanveer84@gmail.com>
2021-11-12 21:07:43 -0800

    This is my first commit to check the sql connection in python

---
+++ pysql/pysql.py
@@ -0,0 +1,4 @@
+# This is the practice to work with sql in python and i can do it fairly
+
+# First we will install sql connector in python by using the following pip command (python -m pip install mysql-connector-python)
+
