3485ac3286dba99bbe3901e5eab8057f96f2806c
codehiker1997 <faizantanveer84@gmail.com>
2021-11-12 23:54:24 -0800

    This is third commit after makiing tables in and execute different command for inserting data and fetching data from database

--- pysql/pysql.py
+++ pysql/pysql.py
@@ -7,8 +7,36 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "1234",
-
+    password = "1234567890F",
+    database = "thisdata"
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO students (name, address) VALUES (%s, %s)"
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+mycursor.executemany(sql, val)
+# This is used to create the database table
+# mycursor.execute("CREATE TABLE employees (name VARCHAR(255), address VARCHAR(255))")
+mydb.commit()
+print(mycursor.rowcount, 'row inserted')
+
+mycursor.execute("SELECT * FROM students")
+
+my_results = mycursor.fetchall()
+for x in my_results:
+    print(x)
