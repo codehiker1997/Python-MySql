eb3c883f496f5206f0f40126478570e9c64856f8
codehiker1997 <faizantanveer84@gmail.com>
2021-11-12 23:34:09 -0800

    This is my commit tochck

--- pysql/pysql.py
+++ pysql/pysql.py
@@ -1,4 +1,14 @@
 # This is the practice to work with sql in python and i can do it fairly
 
 # First we will install sql connector in python by using the following pip command (python -m pip install mysql-connector-python)
+# First of all import mysql connector 
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "1234",
+
+)
+
+print(mydb)
